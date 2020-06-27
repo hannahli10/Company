c25f03bec349d89dde68aa1ceb4676dd63859df2
hannah <hannah@hannahdeMacBook-Pro.local>
2020-06-27 15:16:36 -0400

    add unit test for departmentDao

--- src/main/java/org/example/repository/DepartmentDaoImpl.java
+++ src/main/java/org/example/repository/DepartmentDaoImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
+@Repository //Bean
 public class DepartmentDaoImpl implements DepartmentDao {   //create
     private Logger logger = LoggerFactory.getLogger(getClass());
 
--- src/test/java/org/example/repository/DepartmentDaoTest.java
+++ src/test/java/org/example/repository/DepartmentDaoTest.java
@@ -16,55 +16,25 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ApplicationBootstrap.class)
-
 public class DepartmentDaoTest {
     @Autowired
     private DepartmentDao departmentDao;
-    @Autowired
-    private EmployeeDao employeeDao;
 
-//    private DepartmentDao departmentDao = new DepartmentDaoImpl();
-//    private EmployeeDao employeeDao = new EmployeeDaoImpl();
-
-    private Employee e1;
-    private Employee e2;
     private Department d1;
     private String depString = "HR1";
 
 
     @Before   //save
     public void setUp() {
-        //logic 1 save record in one side
-        // departmentDao = new DepartmentDaoImpl();
-        // employeeDao = new EmployeeDaoImpl();
         d1 = new Department();
         d1.setName(depString);
         d1.setDescription("random description");
         d1.setLocation("US");
         departmentDao.save(d1);
-        // d1.setEmployee([feixiong,xiaolu])
-        //d1.setEmployee([ryo])
-        //d1.setEmployees();
-        //logic 2 save record in many side
-        e1 = new Employee();
-        e1.setName("zhang3");
-        e1.setAddress("us");
-//        //update employees set department_id=1 where employee.name='zhang3';
-//        //e1.setDepartment_id(d1.getId);
-        e1.setDepartment(d1);
-        employeeDao.save(e1,Long.valueOf(1));
-        e2 = new Employee();
-        e2.setName("li4");
-        e2.setDepartment(d1);
-        employeeDao.save(e2,Long.valueOf(2));
     }
 
     @After     //delete
     public void tearDown() {
-        //logic 1 delete record in many owning side
-        employeeDao.delete(e1);
-        employeeDao.delete(e2);
-        //logic 2 delete record in one inverse side
         departmentDao.delete(d1);
     }
 
@@ -80,7 +50,7 @@ public class DepartmentDaoTest {
 //    public void getDepartmentEagerByTest(){
 //        Department department = departmentDao.getDepartmentEagerBy(d1.getId());
 //        assertNotnull(department);
-//        AassertEquals(department);
+//        assertEquals(department);
 //        assertEquals(department.getEmployees().size()>0);
 //    }
 }
