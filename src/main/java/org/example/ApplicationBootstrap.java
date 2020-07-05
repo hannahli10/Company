67d4de52dc430ea0401654784450cb1952fa4b5f
hannah <hannah@hannahdeMacBook-Pro.local>
2020-07-05 18:18:10 -0400

    ApplicationBootstrap

--- src/main/java/org/example/ApplicationBootstrap.java
+++ src/main/java/org/example/ApplicationBootstrap.java
@@ -2,11 +2,11 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
 @SpringBootApplication(scanBasePackages = {"org.example"})
+//@ServletComponentScan(basePackages = {"org.example"}) // Need
 public class ApplicationBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationBootstrap.class, args);
