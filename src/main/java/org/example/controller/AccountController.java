d104b6a17aa0e81f9b48ed324b57c07f19973b07
hannah <hannah@hannahdeMacBook-Pro.local>
2020-07-05 18:20:46 -0400

    update Controller

--- src/main/java/org/example/controller/AccountController.java
+++ src/main/java/org/example/controller/AccountController.java
@@ -5,10 +5,9 @@ import org.example.service.AccountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/account")
@@ -18,14 +17,23 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
     @RequestMapping(value ="", method = RequestMethod.GET)
-    public void getAccounts(){
+    public List<Account> getAccounts(){
         logger.debug("i am in the account controller");
+        return accountService.getAccounts();
     }
     //account/1 GET
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Account getAccountById(@PathVariable(name="id") Long id) {
         logger.debug("i am in the account controller get by" + id);
         return accountService.getBy(id);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PATCH)
+    public Account updateAccount(@PathVariable("id")Long id, @RequestParam("AccountType")String AccountType){
+        Account a = accountService.getBy(id);
+        a.setAccountType(AccountType);
+        a = accountService.update(a);
+        return a;
     }
 }
 
--- src/main/java/org/example/controller/DepartmentController.java
+++ src/main/java/org/example/controller/DepartmentController.java
@@ -7,17 +7,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/department")
+
 public class DepartmentController {
     private Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private DepartmentService departmentService;
     @RequestMapping(value ="", method = RequestMethod.GET)
-    public void getDepartments(){
-        logger.debug("i am in the department controller");
+    public List<Department> getDepartments(){
+        logger.debug("i am in the account controller");
+        return departmentService.getDepartments();
     }
     //department/1 GET
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -25,12 +27,12 @@ public class DepartmentController {
         logger.debug("i am in the department controller get by" + id);
         return departmentService.getBy(id);
     }
-//    //
-//    @RequestMapping(value = "/{id}",method = RequestMethod.PATCH)
-//    public Department updateDepartment(@PathVariable("id")Long id,@RequestParam("name")String name){
-//        Department d = departmentService.getBy(id);
-//        d.setName(name);
-//        d = departmentService.update(d);
-//        return d;
-//    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PATCH)
+    public Department updateDepartment(@PathVariable("id")Long id,@RequestParam("name")String name){
+        Department d = departmentService.getBy(id);
+        d.setName(name);
+        d = departmentService.update(d);
+        return d;
+    }
 }
