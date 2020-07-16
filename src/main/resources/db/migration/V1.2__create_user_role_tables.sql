57de7695c75dacca2dcc9a95ef4d9815eff16b3b
hannah <hannah@hannahdeMacBook-Pro.local>
2020-07-15 23:11:25 -0400

    add SQL V1.1 V1.2

---
+++ src/main/resources/db/migration/V1.2__create_user_role_tables.sql
@@ -0,0 +1,32 @@
+CREATE SEQUENCE user_id_seq START WITH 1;
+CREATE SEQUENCE role_id_seq START WITH 1;
+CREATE TABLE users (
+    id              BIGSERIAL NOT NULL,
+    name            VARCHAR(30) not null unique,
+    password        VARCHAR(64),
+    secret_key      varchar(512),
+    first_name      VARCHAR(30),
+    last_name       VARCHAR(30),
+    email           VARCHAR(50) not null unique
+);
+ALTER TABLE users ADD CONSTRAINT users_pk PRIMARY KEY ( id );
+CREATE TABLE role (
+    id                   BIGSERIAL NOT NULL,
+    name                 VARCHAR(30) not null unique,
+    allowed_resource     VARCHAR(200),
+    allowed_read         boolean not null default false,
+    allowed_create       boolean not null default false,
+    allowed_update       boolean not null default false,
+    allowed_delete       boolean not null default false
+);
+ALTER TABLE role ADD CONSTRAINT role_pk PRIMARY KEY ( id );
+CREATE TABLE users_role (
+    user_id    bigint NOT NULL,
+    role_id    bigint NOT NULL
+);
+ALTER TABLE users_role
+    ADD CONSTRAINT users_fk FOREIGN KEY ( user_id )
+        REFERENCES users ( id );
+ALTER TABLE users_role
+    ADD CONSTRAINT role_fk FOREIGN KEY ( role_id )
+        REFERENCES role ( id );
