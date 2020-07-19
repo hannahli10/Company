faa8998dacab92c3912d7ac56b32f5f7dfbf5de5
hannah <hannah@hannahdeMacBook-Pro.local>
2020-07-19 19:01:51 -0400

    add JWTService

---
+++ src/main/java/org/example/service/JWTService.java
@@ -0,0 +1,47 @@
+package org.example.service;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.example.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JWTService {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private final String SECRET_KEY = System.getProperty("secret.key");
+    private final String ISSUER = "org.example";
+    private final long EXPIRATION_TIME = 86400 *1000;
+    public String generateToken(User user) {
+        //JWT signature algorithm using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        //Sign JWT with SECRET_KEY
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        Claims claims = Jwts.claims();
+        claims.setId(String.valueOf(user.getId()));
+        claims.setSubject(user.getName());
+        claims.setIssuedAt(new Date(System.currentTimeMillis()));
+        claims.setIssuer(ISSUER);
+        claims.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME));
+        //Set the Jwt Claims
+        JwtBuilder builder = Jwts.builder().setClaims(claims).signWith(signatureAlgorithm, signingKey);
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+    public Claims decryptJwtToken(String token){
+        Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY)).parseClaimsJws(token).getBody();
+        logger.warn("Claims:" + claims.toString());
+        return claims;
+
+    }
+}
