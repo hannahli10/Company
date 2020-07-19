c8202877f036ac2a1b6965105901c9ad6a6d7ac4
hannah <hannah@hannahdeMacBook-Pro.local>
2020-07-19 19:02:11 -0400

    add JWTServiceTest

---
+++ src/test/java/org/example/repository/JWTServiceTest.java
@@ -0,0 +1,44 @@
+package org.example.repository;
+
+import io.jsonwebtoken.Claims;
+import org.example.ApplicationBootstrap;
+import org.example.model.User;
+import org.example.service.JWTService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationBootstrap.class)
+public class JWTServiceTest {
+    @Autowired
+    private JWTService jwtService;
+
+    @Test
+    public void generateTokenTest(){
+        User u = new User();
+        u.setId(1L);
+        u.setName("Hannah");
+        String token = jwtService.generateToken(u);
+//        assertion
+        assertNotNull(token);
+        }
+
+//    @Test
+//    public void decryptJwtToken(String token){
+//        User u = new User();
+//        u.setId(2L);
+//        u.setName("Li");
+//        String token = jwtService.generateToken(u);
+//        Claims claims = jwtService.decryptJwtToken(token);
+//        assertNotNull(claims);
+//        assertEquals(claims.getSubject(),"Li");
+//    }
+
+
+}
